22ada709cde4c2304f7afd273cb4a8754d0ab6fd
mdlawrenceusa <mdlawrence@me.com>
2015-09-08 11:37:57 -0400

    Updated MyTube

--- moviedat/db/schema.rb
+++ moviedat/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903134938) do
+ActiveRecord::Schema.define(version: 20150908143130) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
